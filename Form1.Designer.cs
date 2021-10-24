00609e72560e1d44b59a13fbf6a9b3fe50d0422c
Malik <Malik@DESKTOP-U898654>
2021-10-24 12:43:00 +0500

    added button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tsBtnLogin = new System.Windows.Forms.ToolStripButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -50,7 +51,7 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // toolStrip1
@@ -69,15 +70,25 @@
             this.tsBtnLogin.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnLogin.Image")));
             this.tsBtnLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsBtnLogin.Name = "tsBtnLogin";
-            this.tsBtnLogin.Size = new System.Drawing.Size(36, 22);
+            this.tsBtnLogin.Size = new System.Drawing.Size(41, 22);
             this.tsBtnLogin.Text = "Login";
             this.tsBtnLogin.Click += new System.EventHandler(this.TsBtnLogin_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Testing";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
@@ -102,6 +113,7 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton tsBtnLogin;
+        private System.Windows.Forms.Button button1;
     }
 }
 
