165cf2e284c001308ffc1b4d0311cfd2f58a2931
Developer <testy755@hotmail.com>
2021-11-17 09:08:29 -0800

    changed code so it can run only one instance of child forms in frmMain

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,10 @@ namespace Cloth
 {
     public partial class frmMain : Form
     {
-        
+        // application forms
+        private frmLogin fl = null;
+        private frmManufactures fm = null;
+
         public frmMain()
         {
             InitializeComponent();
@@ -19,14 +22,29 @@ namespace Cloth
 
         private void TsBtnLogin_Click(object sender, EventArgs e)
         {
-            
-
-            frmLogin fl = new frmLogin();
-            fl.MdiParent = frmMain.ActiveForm;
-            fl.Show();            
+            // runs only one instance of form
+            if (!CheckForDuplicate(fl))
+            {
+                fl = new frmLogin();
+                fl.MdiParent = this;
+                fl.Show();
+               
+            }
         }
 
-        
+        private bool CheckForDuplicate(Form newForm)
+        {
+            bool bValue = false;
+            foreach (Form fm in this.MdiChildren)
+            {
+                if (fm.GetType() == newForm.GetType())
+                {
+                    fm.Activate();
+                    bValue = true;
+                }
+            }
+            return bValue;
+        }
         private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
@@ -34,9 +52,13 @@ namespace Cloth
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            frmManufactures fm = new frmManufactures();
-            fm.MdiParent = frmMain.ActiveForm;
-            fm.Show();
+            // runs only one instance of form
+            if (!CheckForDuplicate(fm))
+            {
+                fm = new frmManufactures();
+                fm.MdiParent = frmMain.ActiveForm;
+                fm.Show();
+            }
         }
     }
 }
